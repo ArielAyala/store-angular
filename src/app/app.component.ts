7502906d28d4d7f3a6b245a3d4ab743815fa7cf9
ArielAyala <arielayalamail@gmail.com>
2020-02-21 07:38:42 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'store-angular';
+  title: 'store-angular';
+
+  items = ['ariel', 'rick', 'john'];
+
+  addItem() {
+    this.items.push("nuevo item")
+  }
 }
