0da42384c38804ee203dbd0d7e487b222f6bc6e1
Ariel Ayala <arielayalamail@gmail.com>
2020-02-23 15:35:59 -0300

    Iterando datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.module'
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,49 @@ export class AppComponent {
   title: 'store-angular';
 
   items = ['ariel', 'rick', 'john'];
+
+  products: Product[] = [{
+    id: '1',
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '2',
+    image: 'assets/images/hoodie.png',
+    title: 'Hoodie',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '3',
+    image: 'assets/images/mug.png',
+    title: 'Mug',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '4',
+    image: 'assets/images/pin.png',
+    title: 'Pin',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '5',
+    image: 'assets/images/stickers1.png',
+    title: 'Stickers',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '6',
+    image: 'assets/images/stickers2.png',
+    title: 'Stickers',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },];
 
   addItem() {
     this.items.push("nuevo item")
