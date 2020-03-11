8d1091c3be1d2cdc084f665d33261c50fd5f8185
ArielAyala <arielayalamail@gmail.com>
2020-03-11 07:06:50 -0300

    Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
 import { ExponentialPipe } from './exponential.pipe';
-import { HighlightDirective } from './highlight.directive'
+import { HighlightDirective } from './highlight.directive';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { DemoComponent } from './demo/demo.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ExponentialPipe,
-    HighlightDirective
+    HighlightDirective,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+
+  title: 'store-angular';
+
+  power = 10;
+
+  items = ['ariel', 'rick', 'john'];
+
+  ngOnInit() {
+
+  }
+
+
+
+  addItem() {
+    this.items.push("nuevo item")
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+}
