735ad224ff79860e8adb68f14ec1c28e25ff11c3
ArielAyala <arielayalamail@gmail.com>
2020-03-12 07:39:27 -0300

    Agregado de imagenes al banner

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
+  images: string[] = [
+    'assets/images/banner-1.jpg',
+    'assets/images/banner-2.jpg',
+    'assets/images/banner-3.jpg'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
