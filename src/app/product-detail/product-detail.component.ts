eb003ced1a119b4187adb6c994efe0d82e77ba4c
Ariel Ayala <arielayalamail@gmail.com>
2020-03-12 20:18:41 -0300

    Rutas con id

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ProductsService } from '../products.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,6 +9,8 @@ import { ProductsService } from '../products.service';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+
+  product: Product;
 
   constructor(
     private route: ActivatedRoute,
@@ -17,9 +20,7 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      const product = this.productsService.getProduct(id);
-      console.log(product);
-
+      this.product = this.productsService.getProduct(id);
     });
   }
 
