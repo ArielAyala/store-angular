efda4f74fb6155ded67d58f91f959dcff1563ddb
ArielAyala <arielayalamail@gmail.com>
2020-03-30 06:19:56 -0400

    Peticiones http a una rest api

--- src/app/product/components/products/products.component.ts
+++ src/app/product/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../product.model';
+import { ProductsService } from './../../../core/services/products/products.service';
 
 @Component({
   selector: 'app-products',
@@ -8,57 +9,26 @@ import { Product } from '../../../product.model';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[] = [{
-    id: '1',
-    image: 'assets/images/camiseta.png',
-    title: 'Camiseta',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '2',
-    image: 'assets/images/hoodie.png',
-    title: 'Hoodie',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '3',
-    image: 'assets/images/mug.png',
-    title: 'Mug',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '4',
-    image: 'assets/images/pin.png',
-    title: 'Pin',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '5',
-    image: 'assets/images/stickers1.png',
-    title: 'Stickers',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  },
-  {
-    id: '6',
-    image: 'assets/images/stickers2.png',
-    title: 'Stickers',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  }];
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.fecthProducts();
   }
 
   clickProduct(id: number) {
     console.log('product');
     console.log(id);
+  }
+
+  fecthProducts() {
+    this.productsService.getAllProducts()
+      .subscribe(products => {
+        this.products = products;
+      })
   }
 
 }
