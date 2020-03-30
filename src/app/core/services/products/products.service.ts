097a947033c60628caf92f9129761f9591150d63
ArielAyala <arielayalamail@gmail.com>
2020-03-30 07:10:45 -0400

    Usando variables de entorno

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../../../product.model';
 import { HttpClient } from '@angular/common/http'
+import { environment } from '../../../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,11 @@ export class ProductsService {
   ) { }
 
   getAllProducts() {
-    return this.http.get<Product[]>('https://platzi-store.herokuapp.com/products/');
+    return this.http.get<Product[]>(environment.url_api + '/products/');
   }
 
   getProduct(id: string) {
-    return this.http.get<Product>(`https://platzi-store.herokuapp.com/products/${id}`);
+    return this.http.get<Product>(`${environment.url_api}/products/${id}`);
   }
 
 }
