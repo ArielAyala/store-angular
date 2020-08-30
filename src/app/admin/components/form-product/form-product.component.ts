d3cffd703f06e3fb0e1f0c04b1722bc9a1f139ce
Ariel Ayala <arielayalamail@gmail.com>
2020-08-30 18:21:30 -0400

    Agregado de validacion custom

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import { Router } from '@angular/router';
+import { MyValidators } from 'src/app/utils/validators';
 
 @Component({
   selector: 'app-form-product',
@@ -27,7 +28,10 @@ export class FormProductComponent implements OnInit {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      price: ['', [Validators.required]],
+      price: ['', [
+        Validators.required,
+        MyValidators.isPriceValid],
+      ],
       image: [''],
       description: ['', Validators.required]
     })
@@ -44,6 +48,10 @@ export class FormProductComponent implements OnInit {
         })
     }
     console.log(this.form.value)
+  }
+
+  get priceField() {
+    return this.form.get('price');
   }
 
 }
