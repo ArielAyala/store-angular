086e154cf75df223f7de2b81d82a5b5201727145
Ariel Ayala <arielayalamail@gmail.com>
2020-09-10 07:39:47 -0400

    Agragado de form login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,12 +21,10 @@ const routes: Routes = [
       },
       {
         path: 'products',
-        canActivate: [AdminGuard],
         loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
       },
       {
         path: 'contact',
-        canActivate: [AdminGuard],
         loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
       },
       {
@@ -35,14 +33,18 @@ const routes: Routes = [
       },
       {
         path: 'demo',
-        canActivate: [AdminGuard],
         loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)
       },
     ]
   },
   {
     path: 'admin',
+    canActivate: [AdminGuard],
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: '**',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  url_api: 'https://platzi-store.herokuapp.com'
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyBXcI2z57w1sgvU7tFUMdAQRvhuYWRlKFs',
+    authDomain: 'store-angular-881f5.firebaseapp.com',
+    databaseURL: 'https://store-angular-881f5.firebaseio.com',
+    projectId: 'store-angular-881f5',
+    storageBucket: 'store-angular-881f5.appspot.com',
+    messagingSenderId: '706602697813',
+    appId: '1:706602697813:web:df2d4bc28b17950cafc0dc'
+  }
 };
 
 /*
