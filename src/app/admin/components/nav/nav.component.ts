d17e41e0c2a3e0890cb553e902485abca66c6ebe
Ariel Ayala <arielayalamail@gmail.com>
2020-09-13 15:36:08 -0400

    guard para controlar acceso al admin

--- src/app/admin/components/nav/nav.component.ts
+++ src/app/admin/components/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -16,6 +18,17 @@ export class NavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  logout() {
+    this.auth.logout()
+      .then(() => {
+        this.router.navigate(['/home']);
+      })
+  }
 
 }
